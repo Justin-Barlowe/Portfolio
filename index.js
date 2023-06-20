4772e7de0d52cd6b7afcc680d5a9f4d6285188fe
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2023-06-19 20:11:55 -0400

    Portfolio in progress

--- index.js
+++ index.js
@@ -12,12 +12,31 @@ app.use('/images', express.static(__dirname + 'public/images'))
 //Set Views
 app.set('views', './views')
 app.set('view engine', 'ejs')
-// app.set('partials', './views/partials')
+
 
 app.get('', (req,res) => {
     res.render('index')
 })
 
+app.get('/about', (req,res) => {
+    res.render('about')
+})
+
+app.get('/projects', (req,res) => {
+    res.render('projects')
+})
+
+app.get('/database', (req,res) => {
+    res.render('database')
+})
+
+app.get('/apitests', (req,res) => {
+    res.render('apitests')
+})
+
+app.get('/resume', (req,res) => {
+    res.render('resume')
+})
 
 //Listen on port 3000
 app.listen(port, () => console.info(`Listening on port ${port}`))
